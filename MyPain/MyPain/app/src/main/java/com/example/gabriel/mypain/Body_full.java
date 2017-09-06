cf9dbf7000f7f8e3d66fd0606099a73489f31e90
New <New>
2017-09-06 08:09:22 -0300

    Changing floating button id

--- MyPain/MyPain/app/src/main/java/com/example/gabriel/mypain/Body_full.java
+++ MyPain/MyPain/app/src/main/java/com/example/gabriel/mypain/Body_full.java
@@ -18,7 +18,7 @@ public class Body_full extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.change_rotation);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
